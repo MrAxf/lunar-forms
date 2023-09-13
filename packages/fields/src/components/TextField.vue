81eb4c7a6cf1bad9c22a8f028da60e3adfbbe41b
Juan Andres <theaxford@gmail.com>
2023-09-13 20:28:50 +0200

    upgrade docs

--- packages/fields/src/components/TextField.vue
+++ packages/fields/src/components/TextField.vue
@@ -1,3 +1,72 @@
+<!-- eslint-disable @typescript-eslint/ban-ts-comment -->
+<!-- eslint-disable vue/no-setup-props-destructure -->
+<!-- eslint-disable vue/require-default-prop -->
+<script setup lang="ts">
+import { inject } from 'vue';
+import type { PluginOptions } from '../types/plugin';
+import type {
+  FieldValue,
+  Maybe,
+  FieldTransformer,
+  FieldValidation,
+} from '@lunar-forms/core';
+import { useField } from '@lunar-forms/core';
+import { PLUGING_CONTEXT_KEY } from '../consts';
+
+const props = withDefaults(
+  defineProps<{
+    name: string;
+    label?: string;
+    help?: string;
+    modelValue?: FieldValue;
+    initialValue?: FieldValue;
+    transform?: Maybe<FieldTransformer[]>;
+    validate?: Maybe<FieldValidation[]>;
+    validateOn?: 'input' | 'change' | 'blur' | null;
+  }>(),
+  {
+    validateOn: 'input',
+  }
+);
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: FieldValue): void;
+  (e: 'blur', ev: FocusEvent): void;
+  (e: 'change', ev: Event): void;
+  (e: 'focus', ev: FocusEvent): void;
+  (e: 'input', ev: InputEvent): void;
+}>();
+
+const fieldData = useField(props.name, {
+  initialValue: props.initialValue,
+  validate: props.validate,
+  validateOn: props.validateOn,
+  transform: props.transform,
+  onblur(ev) {
+    emit('blur', ev);
+  },
+  onchange(ev) {
+    emit('change', ev);
+  },
+  onfocus(ev) {
+    emit('focus', ev);
+  },
+  oninput(ev) {
+    // @ts-ignore
+    emit('update:modelValue', ev.target?.value);
+    emit('input', ev);
+  },
+});
+
+const options = inject<PluginOptions>(PLUGING_CONTEXT_KEY);
+
+if (!options) throw new Error('Lunar Forms Fields plugin is not setted.');
+
+const { theme } = options;
+
+const id = `${props.name}-${crypto.randomUUID()}`;
+</script>
+
 <template>
   <div :class="theme.classes.outer">
     <div :class="theme.classes.wrapper">
@@ -10,27 +79,7 @@
         <div :class="theme.classes.suffix"></div>
       </div>
     </div>
-    <span :class="theme.classes.help"></span>
+    <span v-if="props.help" :class="theme.classes.help">{{ props.help }}</span>
     <span :class="theme.classes.message"></span>
   </div>
 </template>
-
-<script setup lang="ts">
-import { inject } from 'vue';
-import type { PluginOptions } from '../types/plugin';
-import { PLUGING_CONTEXT_KEY } from '../consts';
-
-const props = defineProps<{
-  name: string;
-  label?: string;
-}>();
-
-const options = inject<PluginOptions>(PLUGING_CONTEXT_KEY);
-
-if (!options) throw new Error('Lunar Forms Fields plugin is not setted.');
-
-const { theme } = options;
-
-// autogenerate id for input
-const id = `${props.name}-${crypto.randomUUID()}`;
-</script>
