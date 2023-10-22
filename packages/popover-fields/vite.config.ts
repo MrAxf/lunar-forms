f7287db9acdc042804cc9cf353b6975da14f1a37
Juan Andres <theaxford@gmail.com>
2023-10-22 19:33:05 +0200

    feat(popover-fields): :tada: Added new package

---
+++ packages/popover-fields/vite.config.ts
@@ -0,0 +1,35 @@
+/// <reference types="vitest" />
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'LunarFormsPopoverFields',
+      fileName: 'index',
+    },
+    sourcemap: mode === 'development',
+    rollupOptions: {
+      external: ['vue', '@lunar-forms/core'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          '@lunar-forms/core': 'LunarFormsCore',
+        },
+      },
+    },
+  },
+  test: {
+    include: ['test/**/*.test.ts'],
+    environment: 'happy-dom',
+  },
+}));
